080c45899a25e39f02e34882e76a73208d83638a
Josh Cheek <josh.cheek@gmail.com>
2012-12-12 02:40:06 -0600

    Remove literal in spec where there ia var for it

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe 'some acceptance specs' do
   specify 'words are returned sorted by length asc and do not include duplicates or previous guesses' do
-    unguessed   = %w[ab abc dac]
+    unguessed   = %w[abc ab dac]
     guessed     = %w[adc dab]
     nonmatching = %w[x nek]
     all_words   = unguessed + guessed + nonmatching
 
     letter_press = LetterPressIsNotAsGoodAsBoggle.new all_words do
       board   %w[a b c d a]
-      guesses %w[adc dab]
+      guesses guessed
     end
 
     letter_press.words.should == %w[ab abc dac]
